e0cbd499193f2d244fbd3411d118bae4cdcd11b8
carlos muleke <carlosmuleke@gmail.com>
2025-06-27 16:41:13 +0200

    portfolio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,16 +7,16 @@ const Footer = () => {
     <footer className="footer">
       <p className="footer__title">Carlos Muleke</p>
       <div className="footer__social">
-        <a href="https://www.linkedin.com/in/carlos-muleke-084552362" className="home__social-icon">
+        <a href="https://www.linkedin.com/in/carlos-muleke-084552362" className="footer__icon">
           <FontAwesomeIcon icon={faLinkedin} style={{ color: '#0077B5' }} />
         </a>
-        <a href="https://github.com/carlos45-dev" className="home__social-icon">
+        <a href="https://github.com/carlos45-dev" className="footer__icon">
           <FontAwesomeIcon icon={faGithub} style={{ color: '#ffffff' }} />
         </a>
-        <a href="https://wa.me/265992779377" className="home__social-icon">
+        <a href="https://wa.me/265992779377" className="footer__icon">
           <FontAwesomeIcon icon={faWhatsapp} style={{ color: '#25D366' }} />
         </a>
-        <a href="https://web.facebook.com/carlos.bathromew" className="home__social-icon">
+        <a href="https://web.facebook.com/carlos.bathromew" className="footer__icon">
           <FontAwesomeIcon icon={faFacebook} style={{ color: '#1877F2' }} />
         </a>
       </div>
