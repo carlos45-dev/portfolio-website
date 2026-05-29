c34562df0f5ef4618ceed6d58b196ba8c58c46da
carlos muleke <mulelecarlos33@gmail.com>
2026-05-29 22:43:46 +0200

    changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -28,11 +28,16 @@ const About = () => {
             transition={{ duration: 0.7, delay: 0.2 }}
             viewport={{ once: true }}
           >
-            A front-end developer with  certifications and a strong passion for
-            building modern, responsive websites. Currently in my third year
-            studying Computer Science at the University of Malawi, I focus on
-            creating clean, user-friendly interfaces while continuously expanding
-            my skills in web development.
+            Passionate and driven 3rd year Computer Science student at 
+            the University of Malawi with hands on experience in full 
+            stack development using React.js, Next.js, Node.js, 
+            and Flutter. I have independently and collaboratively 
+            built and deployed real world web applications. 
+            I am an active member of the university's Computing Society, 
+            committed to leveraging technology to solve meaningful problems 
+            at a local and international level. I thrive in collaborative 
+            environments and am constantly expanding my skills to become a 
+            globally competitive software developer.
           </motion.p>
         </div>
       </div>
